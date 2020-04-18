ee20fb9273c274bf53d7834d6c4ec04cf4635cc9
Lars-Kristian Dahl <lars.dahl@gmail.com>
2020-04-18 11:40:56 +0200

    Expose StripeApiException
    Fix #39

--- lib/stripe_sdk.dart
+++ lib/stripe_sdk.dart
@@ -1,6 +1,7 @@
 library stripe_sdk;
 
-export './src/customer_session.dart';
-export './src/stripe.dart';
-export './src/stripe_api.dart';
-export './src/stripe_api_handler.dart';
+export 'src/customer_session.dart';
+export 'src/stripe.dart';
+export 'src/stripe_api.dart';
+export 'src/stripe_api_handler.dart';
+export 'src/stripe_error.dart';
