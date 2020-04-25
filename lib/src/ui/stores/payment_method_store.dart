e810428f9bde530eb0b86f458e533dcaf9d21ac3
Lars-Kristian Dahl <lars.dahl@gmail.com>
2020-04-26 00:38:58 +0200

    Add documentation

--- lib/src/ui/stores/payment_method_store.dart
+++ lib/src/ui/stores/payment_method_store.dart
@@ -1,10 +1,18 @@
+import 'dart:async';
+
 import 'package:flutter/widgets.dart';
 
 import '../../customer_session.dart';
 import '../screens/payment_methods_screen.dart';
 
+/// A managed repository for payment methods.
+/// This is the preferred way to work with payment methods when using Flutter.
+/// It will dispatch change notifications to listeners as necessary.
+///
 class PaymentMethodStore extends ChangeNotifier {
   final List<PaymentMethod> paymentMethods = List();
+
+  /// The customer session the store operates on.
   final CustomerSession _customerSession;
 
   PaymentMethodStore({CustomerSession customerSession})
@@ -17,22 +25,21 @@ class PaymentMethodStore extends ChangeNotifier {
     if (isFirstListener) refresh();
   }
 
-  void clear() {
-    paymentMethods.clear();
-  }
-
+  /// Attach a payment method and refresh the store if there are any active listeners.
   Future<Map> attachPaymentMethod(String paymentMethodId) {
     final paymentMethodFuture = _customerSession.attachPaymentMethod(paymentMethodId);
     refresh();
     return paymentMethodFuture;
   }
 
+  /// Detach a payment method and refresh the store if there are any active listeners.
   Future<Map> detachPaymentMethod(String paymentMethodId) {
     final paymentMethodFuture = _customerSession.detachPaymentMethod(paymentMethodId);
     refresh();
     return paymentMethodFuture;
   }
 
+  /// Refresh the store if there are any active listeners.
   Future<void> refresh() {
     if (!hasListeners) return Future.value();
 
