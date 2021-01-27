2955d9e06b31a4934c255d3aa43c87ea5aaa76bf
francesco romeo <f.romeo.f@gmail.com>
2021-01-27 17:52:28 +0100

    fixing issue "credit_card_validator has broken stripe-sdk #111", we eliminated the validation of the cvc only, if needed we can put a simple number and maxChar validation inline.

--- lib/src/models/card.dart
+++ lib/src/models/card.dart
@@ -41,7 +41,7 @@ class StripeCard {
   bool validateDate() {
     return _ccValidator
         .validateExpDate(
-            '${expMonth.toString().padLeft(2, '0')}/${expYear.toString().padLeft(2, '0')}')
+        '${expMonth.toString().padLeft(2, '0')}/${expYear.toString().padLeft(2, '0')}')
         .isValid;
   }
 
@@ -50,12 +50,7 @@ class StripeCard {
   /// @return {@code true} if valid, {@code false} otherwise
   bool validateCVC() {
     if (cvc == null) return false;
-    if (number != null) {
-      final cardType = _ccValidator.validateCCNum(number).ccType;
-      return _ccValidator.validateCVV(cvc, cardType: cardType).isValid;
-    } else {
-      return _ccValidator.validateCVV(cvc).isValid;
-    }
+    return _ccValidator.validateCVV(cvc, _ccValidator.validateCCNum(number).ccType).isValid;
   }
 
   /// Returns a stripe hash that represents this card.
