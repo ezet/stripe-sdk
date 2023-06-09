0782316c887277b9d37033abb463fa6d7ceb0a09
samil <studi.msa@gmail.com>
2023-06-09 07:25:48 +0300

    libraries updated

--- lib/src/ui/stripe_web_view.dart
+++ lib/src/ui/stripe_web_view.dart
@@ -2,25 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class StripeWebView extends StatelessWidget {
-  const StripeWebView({Key? key, required this.uri, required this.returnUri}) : super(key: key);
+  const StripeWebView({Key? key, required this.uri, required this.returnUri})
+      : super(key: key);
 
   final String uri;
   final Uri returnUri;
 
   @override
   Widget build(BuildContext context) {
-    return WebView(
-        initialUrl: uri,
-        javascriptMode: JavascriptMode.unrestricted,
-        navigationDelegate: (navigation) {
+    WebViewController controller = WebViewController()
+      ..loadRequest(Uri.parse(uri))
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(NavigationDelegate(
+        onNavigationRequest: (navigation) {
           final uri = Uri.parse(navigation.url);
           if (uri.scheme == returnUri.scheme &&
               uri.host == returnUri.host &&
-              uri.queryParameters['requestId'] == returnUri.queryParameters['requestId']) {
+              uri.queryParameters['requestId'] ==
+                  returnUri.queryParameters['requestId']) {
             Navigator.pop(context, true);
             return NavigationDecision.prevent;
           }
           return NavigationDecision.navigate;
-        });
+        },
+      ));
+    return WebViewWidget(
+      controller: controller,
+    );
   }
 }
